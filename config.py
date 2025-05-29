c9e7b58a915b3542641063e8dad3f069a0891c6d
medpirate21 <medoracle@proton.me>
2025-05-29 10:22:49 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,37 @@
+# Telegram API credentials
+TOKEN = "7466365111:AAFwKlvKd8dcIlud3fl2XM1sJY-Da2tPN1E"
+HASH = "4e42d7f5a246adc4af1d2b1ee4165901"
+ID = "28985353"
+# STRING = "BQG6SAkAd-HZnGl0f1cVLRiC2_s2mdo_9THpJrpcMUg6MrljwVQU9NrVPh-E3nA40p2bMWdvxZgOCHuulwRwl-QDpaR8X0ndFEWaJ5ydw-4uwF9dwkBjr-TsN1_rbnPs5pHh5p50iQ2ORRDU15RAzXW-RWsX8huKsn6v_AwqGTGe0-ROxt3fVaIH3DJnCXddE50MfvX9dt1Gy2_XM-tRVZyilEXc-2VkJQOT1cP2TslYqklNU1j8Fk1G6UGTrfmpg9BPF2ZMCi5V2wwrq3cNBNp3q0QSuUlnVhsFN0dLo7hGa-Jau4PZN7fFI9zuvViGWfkOvQvSPfkSoZUkG6h6vOL3xRDKvgAAAAHAZLXpAA" # Your session string (optional)
+
+# MongoDB Atlas connection string
+MONGODB_URI = "mongodb+srv://muthu2:muthu2@cluster0.u0sdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Usage message
+
+
+# DUMP_CHANNEL_ID
+DUMP_CHANNEL_ID = -1002589598140  # Replace with your dump channel ID
+
+# Usage message
+USAGE = """
+**Public Channels:**
+`Simply send the post link`
+
+**Private Channels:**
+1. `/signin` and authorize the bot
+2. Send the invite link (if not member)
+3. Send post link
+
+**Bot Messages:**
+Send link with format:
+`https://t.me/b/botusername/123`
+
+**Batch Processing:**
+Use format "from-to" in link:
+`https://t.me/xyz/100-120`
+
+**Media Groups:**
+Add ?single to link:
+`https://t.me/xyz/100?single`
+"""
